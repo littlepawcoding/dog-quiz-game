1dfebe515fbf2e78f68310b8e2e1be48c8de1c3f
yifaya <64800978+yifaya@users.noreply.github.com>
2020-06-18 13:02:44 +0100

    JS + HTML changes
    Added some initial JS functions as well as a line of HTML

---
+++ js/script.js
@@ -0,0 +1,42 @@
+//CANVAS
+
+var canvas = document.querySelector("#screen");
+canvas.width = 1000;
+canvas.height = 1000;
+
+var ctx = canvas.getContext("2d");
+
+// var maxheight = canvas.height * 0.3;
+
+//STATE
+
+var state = {
+
+}
+
+//BACKGROUND
+
+var background=document.querySelector("#background");
+
+function drawBackground() {
+var width = canvas.width;
+var height = canvas.height;
+ctx.drawImage(background, 0, 0, width, height);
+}
+
+//DOGGIE
+
+var doggie=document.querySelector("#doggie");
+
+function drawDug() {
+var width = 20;
+var height = 20;
+ctx.drawImage(doggie, 100, 100, width, height);
+}
+
+//game
+
+function runGame() {
+drawBackground()
+drawDug()
+}
